7707ddeebefb6976d984dcada956520e9c33315a
Kattoor <jasper.catthoor@gmail.com>
2020-12-01 21:42:50 +0100

    Lobbies query now returns percentageFilled as an integer

--- server/lobbies.sql
+++ server/lobbies.sql
@@ -9,5 +9,5 @@ SELECT dsc,
        "redTeam",
         "countryFlag",
        "steamStartUrl",
-       round((json_array_length("blueTeam") + json_array_length("redTeam")) / "maxPlayer"::dec * 100, 2) as percentageFilled
+       round((json_array_length("blueTeam") + json_array_length("redTeam")) / "maxPlayer"::dec * 100)::int as "percentageFilled"
 FROM lobbies
